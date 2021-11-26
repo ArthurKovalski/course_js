95353aedc1b5fcf3f303ffd1a00f9e064ab5fcb4
ArthurKovalski <arthurkovalski987@gmail.com>
2021-11-26 17:09:15 -0500

    feat: added a new lesson

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
+
+// Lesson number ONE
+
+
+
 // let a = 'Nikita';
 // let b = '37';
 // let z7 = 'Libra';
@@ -8,19 +13,41 @@
 // console.log(c[5]);
 // console.log(c);
 // console.log(c.length);
-console.log('Hello World');
-console.info('Hello Arthur')
+// console.log('Hello World');
+// console.info('Hello Arthur')
 
-document.getElementById('out').innerHTML = 'Hello';
-document.getElementById('out').innerHTML = '2021';
-document.getElementById('out').innerHTML = '<b>2021</b>'; 
-document.querySelector('#one').innerHTML = '989898989';
-document.querySelector('.link').innerHTML = '45454545454';
+// document.getElementById('out').innerHTML = 'Hello';
+// document.getElementById('out').innerHTML = '2021';
+// document.getElementById('out').innerHTML = '<b>2021</b>'; 
+// document.querySelector('#one').innerHTML = '989898989';
+// document.querySelector('.link').innerHTML = '45454545454';
 
-let  b;
-let a = document.querySelector('#one');
-let c;
-c = document.querySelector('.link');
+// let  b;
+// let a = document.querySelector('#one');
+// let c;
+// c = document.querySelector('.link');
 
-a.innerHTML = 'Hello man! You are rock!';
-c.innerHTML = 'I got it'
+// a.innerHTML = 'Hello man! You are rock!';
+// c.innerHTML = 'I got it'
+
+
+
+// lesson NUMBER TWO
+
+document.querySelector('.input__in');// input
+
+let inputIn = document.querySelector('.input__in');
+
+let button = document.querySelector('button');//button
+
+let div = document.querySelector('.out');
+
+button.onclick = function () {
+//when will push button
+console.log('Hey, everything is good!');
+// console.log(inputIn.value); //value is what inside input
+let b = +inputIn.value;//+ turns into a number!!!!!!
+console.log(b + 10);//'55' + 10 = 5510
+div.innerHTML = b;
+inputIn.value = ''; // clean input after action
+}
