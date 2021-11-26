7de43dcd01bd42c1975b4d1056f5a34580991ada
ArthurKovalski <arthurkovalski987@gmail.com>
2021-11-26 13:39:21 -0500

    feat: study new course

---
+++ js/index.js
@@ -0,0 +1,26 @@
+// let a = 'Nikita';
+// let b = '37';
+// let z7 = 'Libra';
+// let c = ['Nikita', 37 , 'Libra'];
+// let d = [];
+// console.log(c[0]);
+// console.log(c[1]);
+// console.log(c[5]);
+// console.log(c);
+// console.log(c.length);
+console.log('Hello World');
+console.info('Hello Arthur')
+
+document.getElementById('out').innerHTML = 'Hello';
+document.getElementById('out').innerHTML = '2021';
+document.getElementById('out').innerHTML = '<b>2021</b>'; 
+document.querySelector('#one').innerHTML = '989898989';
+document.querySelector('.link').innerHTML = '45454545454';
+
+let  b;
+let a = document.querySelector('#one');
+let c;
+c = document.querySelector('.link');
+
+a.innerHTML = 'Hello man! You are rock!';
+c.innerHTML = 'I got it'
