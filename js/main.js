062f97075480472ce0656774d5d1f756252696f7
ArthurKovalski <arthurkovalski987@gmail.com>
2021-12-06 19:49:49 -0500

    feat:continue object

--- js/main.js
+++ js/main.js
@@ -24,3 +24,64 @@
 
 // Objects
 //  _____________________________________
+
+let user = {
+    name:'Jone',
+    age: 30,
+    car: 'Toyota',
+    'like birds':true,
+    beer: 'Corona',
+    vodka: 'Stolichnaya'
+}
+
+user.name = 'Vasya'
+delete user.name
+console.log(user)
+
+// // let key = 'like birds';
+// // console.log(user[key])
+
+// //  delete user.vodka;
+// // user['like birds'] = false
+// // user.name = 'Vasya';
+// // console.log(user['like birds'])
+// // console.log(user);
+
+// //This variable to get property from object. 
+// let key = prompt('What do you want to know?', ); 
+
+// alert(user[key]);
+
+
+// let fruit = prompt('What kind fruit to buy?', 'apple');
+
+// let bag = {
+// //     [fruit]: 5,
+// // };
+
+// // alert(bag.apple)
+
+// for(let key in user){
+// // console.log('women' in user);
+// // console.log('name' in user);
+
+// // console.log(key);
+// console.log(user[key]);
+// }
+
+
+let salaries = {
+    Jonh: 100,
+    Vasya: 200,
+    Kolya: 50,
+    Sanek: 50,
+}
+
+// let sum = salaries.Jonh + salaries.Vasya + salaries.Kolya + salaries.Sanek;
+// console.log(sum)
+
+let sum = 0;
+for(let key in salaries){
+    sum += salaries[key]
+}
+console.log(sum)
