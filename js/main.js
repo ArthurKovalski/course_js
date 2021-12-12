d94dc2ea20ec318b41622036f2710eaa05d84f82
ArthurKovalski <arthurkovalski987@gmail.com>
2021-12-11 19:25:16 -0500

    feat: add

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 document.querySelector('button').onclick = ()=>{
 console.log(document.querySelector('#one') .value);
+
+
+document.querySelector('button').style.backgroundColor =  
+document.querySelector('#one') .value;
 }
 
 
