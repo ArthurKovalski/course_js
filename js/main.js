2e999ec0b5152d0dcab7af4db9d97aab7ce34452
ArthurKovalski <arthurkovalski987@gmail.com>
2021-12-17 18:57:41 -0500

    feat:loops

--- js/main.js
+++ js/main.js
@@ -44,13 +44,13 @@ console.log(div)
 // div.style.background = 'red'
 
 for(let i = 0; i < div.length; i++){
-     console.log(i)
-     
+     console.log(div[i]);
+     div[i].style.background = 'red';
      
 
 }
 
-
+fu
 
 
 
