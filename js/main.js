3d57e6194478f37cca8e497d329e8c10130741ba
ArthurKovalski <arthurkovalski987@gmail.com>
2022-02-03 18:36:15 -0500

    feat: array

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
+const student = {
+    name:'Valera',
+    email:'valer@gmail.com',
+    score: {
+        task1:90,
+        task2:50
+    },
+    1: 10,
+    'dumb key':24,
+}
+const keys = Object.keys(student);
+const values = Object.values(student);
+console.log(student['dumb key']);
+console.log(student.score);
+console.log(student['1'])
 
-
-
-
+// const array = [1,'string',{student},['1',2]];
+// const value = array[1];
+// console.log(value);
+const functionA = (a) => a;
+student[functionA(12)] = 12
+student.newKey = 'newValue';
+console.log(student)
